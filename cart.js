4312f068e51a508a363b1ae9ef3b2c35b4b51988
lintu22 <l.e.richards@live.co.uk>
2022-02-13 14:17:58 +0000

    init

---
+++ cart.js
@@ -0,0 +1,30 @@
+
+// TO DO
+    
+    // 1. Create object for product details
+
+    const items = {
+        A: {},
+    }
+        
+    // a. add boolean to show if product is on offer.
+    // b. add offer details which will be triggered later e.g. threshold and new price
+           
+
+
+    /* 2. Create functionality for add/remove cart
+        a. Add to bag button adds product
+        b. Remove from cart button to remove
+    */
+
+
+
+    // 3. Functionality to check and update items of offer
+    // a. is 1a true? do b.
+    // b. check 1b. offer details. What is the threshold for offer to trigger? Should trigger on multiple of this number above 0
+
+
+
+    // 4. Calculate totals
+
+
