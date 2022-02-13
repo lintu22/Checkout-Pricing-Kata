b7ac5b396ecc9ca9c2a783a496997afc30c47413
lintu22 <l.e.richards@live.co.uk>
2022-02-13 14:33:19 +0000

    Added item details

--- cart.js
+++ cart.js
@@ -4,13 +4,51 @@
     // 1. Create object for product details
 
     const items = {
-        A: {},
+        A: {
+            name: 'Apple',
+            price: 0.5,
+            image: '',
+            // a. add boolean to show if product is on offer.
+            onOffer: true,
+            offerDetails: {
+                 // b. add offer details which will be triggered later e.g. threshold and new price
+                threshold: 3,
+                newPrice: 130,
+            }
+        },
+        B: {
+            name: 'Banana',
+            price: 0.3,
+            image: '',
+            onOffer: true,
+            offerDetails: {
+                threshold: 2,
+                newPrice: 45,
+            }
+        },
+        C: {
+            name: 'Orange',
+            price: 0.2,
+            image: '',
+            onOffer: false,
+            offerDetails: {
+                threshold: 0,
+                newPrice: 0,
+            }
+        },
+        D: {
+            name: 'Pear',
+            price: 0.15,
+            image: '',
+            onOffer: false,
+            offerDetails: {
+                threshold: 0,
+                newPrice: 0,
+            }
+        },
+        
     }
         
-    // a. add boolean to show if product is on offer.
-    // b. add offer details which will be triggered later e.g. threshold and new price
-           
-
 
     /* 2. Create functionality for add/remove cart
         a. Add to bag button adds product
